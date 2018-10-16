fbb65c7a9053cd621271852b4d3187be1c05114c
Wojciech Szućko <wojciech.szucko@gmail.com>
2018-10-16 22:00:00 +0200

    feat: add content to history view

--- src/views/history/History.js
+++ src/views/history/History.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Operation } from '../../components/history/operation/Operation';
 
 export class History extends React.Component {
 	render() {
 		return (
-			<div>History view works!</div>
+			<div className="wrapper">
+				<h3>Aktualny stan budżetu: 1000</h3>
+				<Operation/>
+			</div>
 		);
 	}
 }
