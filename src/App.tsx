dda3abde6114262da65402e5cfa281ea81a5114d
Wojciech Szućko <wojciech.szucko@gmail.com>
2021-07-28 22:37:36 +0200

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import { BrowserRouter as Router, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import {
+	openAddGoalDialogAction,
+	openAddOperationDialogAction,
+	openAuthorDialogAction
+} from './store/actions/dialogs.actions';
+import { Fab, Icon, Menu, MenuItem } from '@material-ui/core';
+import { Goals } from './views/Goals/Goals';
+import { History } from './views/History/History';
+import { DialogsContainer } from './components/dialogs/DialogsContainer/DialogsContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App: React.FC = () => {
+	const [anchorEl, setAnchorEl] = useState<any>();
+	const dispatch = useDispatch();
+
+	const openMenu = (event: any) => setAnchorEl(event.currentTarget);
+
+	const closeMenu = () => setAnchorEl(null);
+
+	const openAddOperationDialog = () => {
+		closeMenu();
+		dispatch(openAddOperationDialogAction());
+	}
+
+	const openAddGoalDialog = () => {
+		dispatch(openAddGoalDialogAction());
+		closeMenu();
+	};
+
+	const openAuthorDialog = () => dispatch(openAuthorDialogAction());
+
+	return (
+		<Router>
+			<>
+				<header>
+					<div className="wrapper">
+						<div className="header-top">
+							<span className="header-top-logo">Household finances</span>
+							<a className="header-top-author" onClick={openAuthorDialog}>Author</a>
+						</div>
+						<div className="header-links">
+							<div className="header-links-date">
+								2021
+							</div>
+							<nav>
+								<ul>
+									<li><NavLink className="link" activeClassName="active"
+												 to="/history">History</NavLink></li>
+									<li><NavLink className="link" activeClassName="active"
+												 to="/goals">Goals</NavLink></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</header>
+
+				<Switch>
+					<Route path="/history" component={History}/>
+					<Route path="/goals" component={Goals}/>
+					<Redirect from="/" to="/history"/>
+				</Switch>
+
+				<Fab color="primary" className="fab" onClick={openMenu}>
+					<Icon>
+						add_circle
+					</Icon>
+				</Fab>
+
+				<Menu
+					id="simple-menu"
+					anchorEl={anchorEl}
+					open={Boolean(anchorEl)}
+					onClose={closeMenu}
+				>
+					<MenuItem onClick={openAddOperationDialog}>
+						Add operation
+					</MenuItem>
+					<MenuItem onClick={openAddGoalDialog}>
+						Add goal
+					</MenuItem>
+				</Menu>
+				<DialogsContainer/>
+			</>
+		</Router>
+	);
 }
-
-export default App;
